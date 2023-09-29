06abcef5327933ef10a0e4f109364924862cac5d
Evgeniy Bukharin <buharinzxc123@gmail.com>
2023-09-29 11:32:07 +0500

    saved

--- src/js/components/custom-scrollbar.js
+++
@@ -1,5 +0,0 @@
-import "simplebar"; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-
-// You will need a ResizeObserver polyfill for browsers that don't support it! (iOS Safari, Edge, ...)
-import ResizeObserver from "resize-observer-polyfill";
-window.ResizeObserver = ResizeObserver;
--- src/js/components/next-button.js
+++
@@ -1,6 +0,0 @@
-const nextButton = document.querySelector(".hero__button-next");
-const cardSection = document.querySelector(".card");
-nextButton.addEventListener("click", (e) => {
-	e.preventDefault();
-	cardSection.classList.add("card-visible");
-});
